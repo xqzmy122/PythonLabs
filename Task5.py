0b867271a2a8e626c2fe244c545eb8d91dec7572
Miron <mironchik515ke@gmail.com>
2024-12-19 18:30:33 +0300

    Task-Manager(last v)

--- Task5.py
+++ Task5.py
@@ -5,77 +5,71 @@ import json
 import os
 app = FastAPI()
 
-FILE_NAME = "tasks.json"
+Json_file = "tasks.json"
 
-if os.path.exists(FILE_NAME):
-    print(f"Файл {FILE_NAME} существует.")
+if os.path.exists(Json_file):
+    print(f"Файл {Json_file} существует.")
 else:
-    print(f"Файл {FILE_NAME} не найден.")
+    print(f"Файл {Json_file} не найден.")
 
-if not os.path.exists(FILE_NAME) or os.path.getsize(FILE_NAME) == 0:
-    # Если файла нет или он пустой, создаём его с пустым JSON
-    with open(FILE_NAME, "w") as file:
+if not os.path.exists(Json_file) or os.path.getsize(Json_file) == 0:
+    with open(Json_file, "w") as file:
         json.dump([], file)
-        print(f"Файл {FILE_NAME} был создан с пустым содержимым.")
+        print(f"Файл {Json_file} был создан с пустым содержимым.")
 else:
-    print(f"Файл {FILE_NAME} уже существует и не пустой.")
+    print(f"Файл {Json_file} уже существует и не пустой.")
 
 class Task(BaseModel): 
     id: int = Field(..., gt=0, description="id должен быть больше 0")
     title: str
     complete: bool = False
 
-# Загрузка задач из файла
-def load_tasks() -> List[dict]:
+class TaskUpdate(BaseModel): 
+    complete: bool = False
+
+def load_tasks() -> List[Task]:
     try:
-        with open(FILE_NAME, "r") as file:
-            return json.load(file)
+        with open(Json_file, "r") as file:
+            return [Task(**task) for task in json.load(file)]
     except FileNotFoundError:
         return []
 
-# Сохранение задач в файл
 def save_tasks(tasks: List[dict]):
-    with open(FILE_NAME, "w") as file:
+    with open(Json_file, "w") as file:
         json.dump(tasks, file, indent=4)
 
-tasks = load_tasks()
+tasks: List[Task] = load_tasks()
+print(tasks)
 
-@app.get("/tasks", response_model=List[Task])
+@app.get("/tasks", response_model = List[Task])
 async def get_tasks() -> List[Task]:
-    return [Task(**task) for task in tasks]
+    return tasks
 
-@app.get("/add",  response_model=Task)
-async def add_task(
-    id: int = Query(..., gt=0, description="Идентификатор задачи (должен быть уникальным)"),
-    title: str = Query(..., min_length=1, description="Название задачи"),
-    complete: bool = Query(False, description="Статус выполнения задачи")
-) -> Task:
-    # Проверка, существует ли задача с таким ID
+@app.post("/add", response_model=Task)
+async def add_task(task: Task) -> Task:
     for existing_task in tasks:
-        if existing_task['id'] == id:
-            raise HTTPException(status_code=400, detail="Задача с таким ID уже существует.")
+        if existing_task.id == task.id:
+            raise HTTPException(status_code=404, detail="Задача с таким ID уже существует.")
     
-    new_task = Task(id=id, title=title, complete=complete)
-    tasks.append(new_task.dict())
+    tasks.append(task.dict())
     save_tasks(tasks)
-    return new_task
+    return task
 
-@app.get("/delete", response_model=int)
+@app.delete("/delete")
 async def delete_task(id: int) -> Task:
-    if id:
-        for task in tasks:
-            if task['id'] == id:
-                tasks.remove(task)
-                save_tasks(tasks)
-                return task
+    for task in tasks:
+        if task.id == id:
+            tasks.remove(task)
+            save_tasks(tasks)
+            return task
     raise HTTPException(status_code=404, detail="Task not found")
 
-@app.get("/update", response_model=Task)
-async def update_task(id: int, complete: bool) -> Task:
-    for task in tasks:
-        if task['id'] == id:
-            task['complete'] = complete
-            save_tasks(tasks)  
-            return task  
-    raise HTTPException(status_code=404, detail="Task not found")    
-
+@app.put("/update", response_model=Task)
+async def update_task(id: int, model: TaskUpdate) -> Task:
+    for idx, task in enumerate(tasks):
+        if task.id == id:
+            updated_task = Task(id=task.id, title=task.title, complete=model.complete)
+            tasks[idx] = updated_task
+            save_tasks([task.dict() for task in tasks])
+            return updated_task
+    raise HTTPException(status_code=404, detail="Task not found")  
